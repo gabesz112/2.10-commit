65131e9f738f314d05dca59aff937114e6c4641c
gaboa <gaborfilus03@gmail.com>
2023-12-13 10:32:43 +0100

    kivonas

--- szamolas.py
+++ szamolas.py
@@ -3,3 +3,9 @@ szam1=int(input("Adj meg egy számot! "))
 szam2=int(input("Adj meg még egy számot! "))
 szam3=szam1+szam2
 print(szam3)
+
+#kivonás
+szam4=int(input("Adj meg egy számot! "))
+szam5=int(input("Adj meg még egy számot! "))
+szam6=szam4+szam5
+print(szam6)
